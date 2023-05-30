9e8f57772542600ecc2c15c786a34c7b1485bb26
pedro9185 <pedrocatarina.ip@gmail.com>
2023-05-30 13:48:06 -0300

    Add-pág-SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,31 +1,35 @@
-import { Button, Grid,  TextField, Typography } from '@mui/material';
+import { Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
 import ButtonDefault from './components/Button';
-import { Link } from 'react-router-dom';
 
-const Login: React.FC= () => {
+
+
+const SignUp: React.FC = () => {
   return (
-     <Grid xs={12} width="100vw" height="100vh" justifyContent="center" alignItems="center" display="flex" bgcolor="#f2f2f2">
+    <React.Fragment>
+      <Grid xs={12} width="100vw" height="100vh" justifyContent="center" alignItems="center" display="flex" bgcolor="#f2f2f2">
               <Grid    height="80%" width="30%" >
               <Grid xs={false}  spacing={2} display="flex" flexDirection="column" bgcolor="#fff" width="100%" height="100%" alignItems="center" justifyContent="center" borderRadius="20px">
                     <Grid xs={12} marginBottom="50px">
-                        <Typography variant='h6'>Acessar App Finanças</Typography>
+                        <Typography variant='h6'>CRIE SUA CONTA</Typography>
                     </Grid>
                    <Grid xs={12} marginBottom="20px">
                      <TextField id="filled-basic" label="Email" type='email' variant="standard" />
                    </Grid>
-                    <Grid xs={12} >
+                    <Grid xs={12} marginBottom="20px">
                      <TextField id="filled-basic" label="Password" type='password' variant="standard" />
                    </Grid>
-                   <ButtonDefault title='Entrar'/>
-                   <Grid xs={12} marginTop='20px'><Link to="/signup">Ainda não tem conta? cadastre-se</Link></Grid>
+                   <Grid xs={12}>
+                     <TextField id="filled-basic" label="Repassword" type='password' variant="standard" />
+                   </Grid>
+                   <ButtonDefault title='Criar conta'/>
                     
                 </Grid>
             </Grid>
      </Grid>
-       
-
+     
+    </React.Fragment>
   );
 };
 
-export default Login;
+export default SignUp;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 
 const AppRoutes: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/home' element={<Home/>}/>
+                <Route path='/signup' element={<SignUp/>}/>
                 <Route path='/' element={<Login/>}/>
             </Routes>
         </BrowserRouter>
