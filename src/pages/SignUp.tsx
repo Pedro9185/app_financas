1d46c73d0534382716bc6de2d7a043e0683f9f5c
Pedro9185 <pedrocatarina.ip@gmail.com>
2023-06-03 10:50:30 -0300

    add-verificaçao-campos-de-cadstro

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,38 @@
 import { Grid, TextField, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ButtonDefault from './components/Button';
 
-
-
 const SignUp: React.FC = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [repassword, setRepassword] = useState("")
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorPassword, setErrorPassword] = useState(false);
+    const [errorRepassword, setErrorRepassword] = useState(false);
+    const [disabled, setDisabled] = useState(false)
+
+    useEffect(() => {
+         const emailValid =
+                (email.endsWith(".com") || email.endsWith(".com.br")) && email.includes("@");
+
+            if (email.length > 0) {
+                setErrorEmail(!emailValid);
+            }
+
+            const passwordValid = password.length >= 6;
+            if (password.length > 0) {
+                setErrorPassword(!passwordValid);
+            }
+
+            const repasswordValid = password === repassword;
+
+            if (repassword.length > 0) {
+                setErrorRepassword(!repasswordValid);
+            }
+
+            setDisabled(!(emailValid && passwordValid && repasswordValid));
+    }, [email, password, repassword])
+
   return (
     <React.Fragment>
       <Grid xs={12} width="100vw" height="100vh" justifyContent="center" alignItems="center" display="flex" bgcolor="#f2f2f2">
@@ -14,13 +42,13 @@ const SignUp: React.FC = () => {
                         <Typography variant='h6'>CRIE SUA CONTA</Typography>
                     </Grid>
                    <Grid xs={12} marginBottom="20px">
-                     <TextField id="filled-basic" label="Email" type='email' variant="standard" />
+                     <TextField id="filled-basic" label="Email" type='email' variant="standard" value={email} onChange={(ev) => setEmail(ev.target.value)}  error = {errorEmail} helperText = {errorEmail ? "Email Invalido" : ""}/>
                    </Grid>
                     <Grid xs={12} marginBottom="20px">
-                     <TextField id="filled-basic" label="Password" type='password' variant="standard" />
+                     <TextField id="filled-basic" label="Password" type='password' variant="standard" value={password} onChange={(ev) => setPassword(ev.target.value)}  error = {errorPassword} helperText = {errorPassword ? "Senha deve conter no minimo 6 caracteres" : ""} />
                    </Grid>
                    <Grid xs={12}>
-                     <TextField id="filled-basic" label="Repassword" type='password' variant="standard" />
+                     <TextField id="filled-basic" label="Repassword" type='password' variant="standard" value={repassword} onChange={(ev) => setRepassword(ev.target.value)} error = {errorRepassword} helperText = {errorRepassword ? "Senhas não coincidem" : ""} />
                    </Grid>
                    <ButtonDefault title='Criar conta'/>
                     
--- src/pages/components/AppBar.tsx
+++ src/pages/components/AppBar.tsx
@@ -4,10 +4,16 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function ButtonAppBar() {
+
+  const navigate = useNavigate()
+  
+ function financas () {
+    navigate('/')
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -15,6 +21,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             App Finanças
           </Typography>
+          <Button color="inherit" onClick={financas}>Login</Button>
           <Button color="inherit">Sair</Button>
         </Toolbar>
       </AppBar>
